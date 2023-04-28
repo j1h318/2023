85adb5d232c07dfea459d581d20f862654766ba7
Sejin Chun <chunsejin@gmail.com>
2023-04-28 22:44:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+st.title ("최고의 수업, OSS 개발에서 배운 Streamlit")
+
+
 df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])
 st.map(df)
